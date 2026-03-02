aaf9bc8483279fd8f970d6c3c3c9b0d61371dc61
Jörgen <jorgen.akesson@regalrexnord.com>
2026-03-02 11:34:19 +0100

    Okta integration

--- Client/src/pages/About.tsx
+++ Client/src/pages/About.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useAuth } from "../contexts/AuthProvider";
 import "./About.css";
 
 function About() {
+  const auth = useAuth();
   return (
     <>
       <div className="about-container">
         <h1>About Page</h1>
+        {auth.isAuthenticated ? (
+          <>
+            <p>{auth.user?.name}</p>
+            <p>{auth.user?.email}</p>
+            <p>{auth.user?.locale}</p>
+            <p>{auth.user?.zoneinfo}</p>
+          </>
+        ) : (
+          <></>
+        )}
         <p>This is the About Page.</p>
       </div>
     </>
