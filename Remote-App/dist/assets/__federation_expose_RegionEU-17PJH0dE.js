b4f40a98f11d8a8cfea9dd668cfb56bde205e9db
Jörgen <jorgen.akesson@regalrexnord.com>
2026-03-03 08:22:18 +0100

    Start

---
+++ Remote-App/dist/assets/__federation_expose_RegionEU-17PJH0dE.js
@@ -0,0 +1,7 @@
+import { j as jsxRuntimeExports } from './jsx-runtime-XI9uIe3W.js';
+
+function Content() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "Region EU" });
+}
+
+export { Content as default };
---
+++ Remote-App/dist/assets/__federation_expose_RegionUS-DI8RoDHs.js
@@ -0,0 +1,7 @@
+import { j as jsxRuntimeExports } from './jsx-runtime-XI9uIe3W.js';
+
+function Content() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("p", { children: "Region US" });
+}
+
+export { Content as default };
